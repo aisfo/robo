29ad5b91282250fd236ee205c32128d540649ef0
Asif Amirguliyev <asif.amirguliyev@gmail.com>
2015-12-06 21:06:25 -0500

    clean up

--- body/body.ino
+++ body/body.ino
@@ -9,136 +9,139 @@
 #define DEGTOSEC 0.0088
 #define PULSELEN 0.00406901042 // 1000 / FREQUENCY / 4096
 
-SoftwareSerial bluetooth(11, 10); // RX, TX
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-Adafruit_BNO055 bno = Adafruit_BNO055(55);
+#define ne0 0
+#define ne1 1
+#define ne2 2
+#define se0 3
+#define se1 4
+#define se2 5
+#define sw0 6
+#define sw1 7
+#define sw2 8
+#define nw0 9
+#define nw1 10
+#define nw2 11
+
+#define MOVE_TO 1
+#define MOVE_BY 2
+
 
 class Joint {
   uint8_t _pin;
   float _zeroPulse;
-  uint16_t _curPos;
   bool _positive;
+  float _currentPosition;
+  static Adafruit_PWMServoDriver _pwm;
 
   public:
-
-    Joint(uint8_t pin, float zeroPulse, bool positive) {
-      _pin = pin;
-      _zeroPulse = zeroPulse;
-      _positive = positive;
-      //_curPos = 0;
+    Joint() {
+      _pin = UINT8_MAX;
     }
 
-    void move(float degree) { 
-      float pulse = degree * DEGTOSEC;
+    Joint(uint8_t pin, float zeroPulse, bool positive, float initialPosition) {
+      _pin = pin;
+      _zeroPulse = zeroPulse;
+      _positive = positive;   
+      _currentPosition = initialPosition;
+    }
+
+    void moveTo(float degree) { 
+      if (_pin == UINT8_MAX) return; //not initialized
+      
+      float pulse = degree * DEGTOSEC / PULSELEN;
       if (!_positive) pulse *= -1;
       
-      pulse /= PULSELEN;
       pulse += _zeroPulse;
   
       if (pulse < SERVOMIN || pulse > SERVOMAX) return;
+
+      _currentPosition = degree;
   
-      pwm.setPWM(_pin, 0, pulse);
+      _pwm.setPWM(_pin, 0, pulse);
+    }
+
+    void moveBy(float degree) { 
+      moveTo(_currentPosition + degree);
+    }
+
+    static void begin() {
+      _pwm.begin();
+      _pwm.setPWMFreq(FREQUENCY);
     }
 };
 
-//void rotate(int newPosition[]) {
-//
-//  boolean done = false;
-//  while (!done) {
-//    done = true;
-//
-//    for (int m = 0; m < 16; m++) {
-//      float curPos = currentPosition[m];
-//      int newPos = newPosition[m];
-//      if (newPos == (int)curPos) continue;
-//
-//      done = false;
-//
-//      float stepSize = 0.5;
-//      if (newPos < curPos) stepSize *= -1;
-//      curPos += stepSize;
-//      currentPosition[m] = curPos;
-//
-//      
-//     // setServoPulse(m, curPos);
-//    }
-//
-//    delay(3);
-//  }
-//
-//}
+Adafruit_PWMServoDriver Joint::_pwm = Adafruit_PWMServoDriver();
+
+SoftwareSerial bluetooth(11, 10); // RX, TX
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+Joint joints[12];
 
 void setup() {
   Serial.begin(9600);
   bluetooth.begin(9600);
-  pwm.begin();
+  Joint::begin();
   bno.begin();
 
-  delay(1000);
-
-  pwm.setPWMFreq(FREQUENCY);
+  delay(500);
+  
   bno.setExtCrystalUse(true);
 
-  Joint nw0(13, 154, true);
-  Joint nw1(14, 565, false);
-  Joint nw2(15, 524, true);
-
-  Joint ne0(2, 565, true);
-  Joint ne1(1, 150, true);
-  Joint ne2(0, 170, false);
-
-  Joint se0(6, 154, true);
-  Joint se1(5, 550, false);
-  Joint se2(4, 535, true);
-
-  Joint sw0(8, 540, true);
-  Joint sw1(9, 170, true);
-  Joint sw2(11, 180, false);
-
-  nw0.move(45);
-  nw1.move(130);
-  nw2.move(-40);
   
-  ne0.move(-45);
-  ne1.move(130);
-  ne2.move(-40);
+  joints[ne0] = Joint(2, 565, true, -45);
+  joints[ne1] = Joint(1, 150, true, 120);
+  joints[ne2] = Joint(0, 170, false, -30);
 
-  se0.move(45);
-  se1.move(130);
-  se2.move(-40);
+  joints[se0] = Joint(6, 154, true, 45);
+  joints[se1] = Joint(5, 550, false, 120);
+  joints[se2] = Joint(4, 535, true, -30);
 
-  sw0.move(-45);
-  sw1.move(130);
-  sw2.move(-40);
+  joints[sw0] = Joint(8, 540, true, -45);
+  joints[sw1] = Joint(9, 170, true, 120);
+  joints[sw2] = Joint(11, 180, false, -30);
+
+  joints[nw0] = Joint(13, 154, true, 45);
+  joints[nw1] = Joint(14, 565, false, 120);
+  joints[nw2] = Joint(15, 524, true, -30);
 }
 
 
 void loop() {
+  //  sensors_event_t event; 
+  //  bno.getEvent(&event);
+    
+  //  Serial.print("Azimuth: ");
+  //  Serial.print(event.orientation.x, 4);
+  //  Serial.print("\tPitch: ");
+  //  Serial.print(event.orientation.y, 4);
+  //  Serial.print("\tRoll: ");
+  //  Serial.print(event.orientation.z, 4);
+  //  Serial.println("");
+  
   if (bluetooth.available()) {
-    int in0 = bluetooth.parseInt();
-    int in1 = bluetooth.parseInt();
-    int in2 = bluetooth.parseInt();
-//    int input[16] = {
-//      0, 0, 0,
-//      0, 0, 0,
-//      0, 0, 0, 0,
-//      0, 0, 0,
-//      in0, in1, in2
-//    };
-//
-//    rotate(input);
+    uint8_t command = bluetooth.parseInt();
+    uint8_t jointId;
+    float value;
+
+    Serial.println(command);
+    
+    switch (command) {
+      case MOVE_TO:
+      case MOVE_BY:
+        jointId = bluetooth.parseInt();
+        value = bluetooth.parseFloat();
+    }
+
+    if (jointId < 0 || jointId > 11) return;
+   
+    switch (command) {
+      case MOVE_TO:
+        joints[jointId].moveTo(value);     
+        break;
+      case MOVE_BY: 
+        joints[jointId].moveBy(value);     
+        break;
+    }
   }
 
-  sensors_event_t event; 
-  bno.getEvent(&event);
-  
-  Serial.print("X: ");
-  Serial.print(event.orientation.x, 4);
-  Serial.print("\tY: ");
-  Serial.print(event.orientation.y, 4);
-  Serial.print("\tZ: ");
-  Serial.print(event.orientation.z, 4);
-  Serial.println("");
-  
   delay(100);
 }
